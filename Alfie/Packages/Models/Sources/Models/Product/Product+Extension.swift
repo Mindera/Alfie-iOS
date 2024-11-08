f0ae50392b08d328da1f0da3758883fbed822cd4
Timea Varga <timea.varga@mindera.com>
2024-11-08 19:47:33 +0200

    ALFMOB-39: Code review, reduced nested if statements

--- Alfie/Packages/Models/Sources/Models/Product/Product+Extension.swift
+++ Alfie/Packages/Models/Sources/Models/Product/Product+Extension.swift
@@ -1,25 +1,16 @@
 public extension Product {
     var priceType: PriceType {
         if let range = priceRange {
-            if let high = range.high {
-                return .range(
-                    lowerBound: range.low.amountFormatted,
-                    upperBound: high.amountFormatted,
-                    separator: "-"
-                )
-            } else {
-                return .default(price: range.low.amountFormatted)
-            }
+            guard let high = range.high else { return .default(price: range.low.amountFormatted) }
+            return .range(lowerBound: range.low.amountFormatted, upperBound: high.amountFormatted, separator: "-")
         } else {
-            if let salePreviousPrice = defaultVariant.price.was {
-                return .sale(
-                    fullPrice: salePreviousPrice.amountFormatted,
-                    finalPrice: defaultVariant.price.amount.amountFormatted
-                )
-            } else {
+            guard let salePreviousPrice = defaultVariant.price.was else {
                 return .default(price: defaultVariant.price.amount.amountFormatted)
             }
+            return .sale(
+                fullPrice: salePreviousPrice.amountFormatted,
+                finalPrice: defaultVariant.price.amount.amountFormatted
+            )
         }
     }
 }
-
