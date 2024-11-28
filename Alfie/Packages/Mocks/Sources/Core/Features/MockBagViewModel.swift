71142061ea547f2a069abbd6b0914f533a327973
Timea Varga <timea.varga@mindera.com>
2024-11-28 22:12:27 +0200

    ALFMOB-44: Fix for mocks

--- Alfie/Packages/Mocks/Sources/Core/Features/MockBagViewModel.swift
+++ Alfie/Packages/Mocks/Sources/Core/Features/MockBagViewModel.swift
@@ -12,8 +12,8 @@ public class MockBagViewModel: BagViewModelProtocol {
         onViewDidAppearCalled?()
     }
 
-    public var onDidSelectDeleteCalled: ((Product) -> Void)?
-    public func didSelectDelete(for product: Product) {
+    public var onDidSelectDeleteCalled: ((SelectionProduct) -> Void)?
+    public func didSelectDelete(for product: SelectionProduct) {
         onDidSelectDeleteCalled?(product)
     }
 }
