a87e2a2972eb80d86d5c73df398c841704a677ed
João Pinho <joao.pinho@mindera.com>
2025-01-10 11:52:09 +0000

    ALFMOB-53: Remove *DependencyContainerProtocol types
    The *DependencyContainerProtocol types do not add value in terms of testability, as they do not mock any behavior. Therefore, we are removing all *DependencyContainerProtocol types and using only the corresponding DependencyContainer structs.

--- Alfie/AlfieKit/Sources/Models/Features/BagDependencyContainerProtocol.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-public protocol BagDependencyContainerProtocol {
-    var bagService: BagServiceProtocol { get }
-}
--- Alfie/AlfieKit/Sources/Models/Features/Search/SearchDependencyContainerProtocol.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public protocol SearchDependencyContainerProtocol {
-    var executionQueue: DispatchQueue { get }
-    var recentsService: RecentsServiceProtocol? { get }
-    var searchService: SearchServiceProtocol { get }
-}
