a1078ce16b06b22a225f52e891bc5ccf57ea6e4b
Daniel Primo <>
2025-01-13 22:40:29 +0000

    % update AlfieKit package.swift file

--- Alfie/AlfieKit/Package.swift
+++ Alfie/AlfieKit/Package.swift
@@ -10,35 +10,6 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Common",
-            targets: ["Common"]
-        ),
-        .library(
-            name: "Core",
-            targets: ["Core"]
-        ),
-        .library(
-            name: "Mocks",
-            targets: ["Mocks"]
-        ),
-        .library(
-            name: "Models",
-            targets: ["Models"]
-        ),
-        .library(
-            name: "Navigation",
-            targets: ["Navigation"]
-        ),
-        .library(
-            name: "StyleGuide",
-            targets: ["StyleGuide"]
-        ),
-        
-        .library(
-            name: "CommonTestUtils",
-            targets: ["CommonTestUtils"]
-        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -71,100 +42,233 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Common",
-            path: "AlfieKit/Common/Sources"
-        ),
-        .target(
-            name: "Core",
-            dependencies: [
-                "Models",
-                "Common",
-                "BFFGraphApi",
-                "EasyStash",
-                .product(name: "FirebaseRemoteConfig", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
-                .product(name: "NukeUI", package: "nuke"),
-                .product(name: "Apollo", package: "apollo-ios"),
-                .product(name: "BrazeKit", package: "braze-swift-sdk"),
-            ],
-            path: "AlfieKit/Core/Sources",
-            resources: [
-            ]
-        ),
-        .target(
-            name: "Mocks",
-            dependencies: [
-                "Models"
-            ],
-            path: "AlfieKit/Mocks/Sources"
-        ),
-        .target(
-            name: "Models",
-            dependencies: [
-                .product(
-                    name: "OrderedCollections",
-                    package: "swift-collections"
-                ),
-            ],
-            path: "AlfieKit/Models/Sources",
-            resources: []
-        ),
-        .target(
-            name: "Navigation",
-            path: "AlfieKit/Navigation/Sources"
-        ),
-        .target(
-            name: "StyleGuide",
-            dependencies: [
-                "Navigation",
-                "Common",
-                "Core",
-                "Models"
-            ],
-            path: "AlfieKit/StyleGuide/Sources",
-            resources: [
-                .copy("Theme/Typography/Resources/SF-Pro-Display-Medium.otf"),
-                .copy("Theme/Components/Loader/spin.gif"),
-                .process("Theme/Typography/Resources/Fonts.xcassets"),
-                .process("Theme/Color/Colors.xcassets"),
-                .process("Theme/Images/ThemedImages.xcassets"),
-                .process("Theme/Toggle/ToggleColor.xcassets")
-            ]
-        ),
-        
-        .target(
-            name: "CommonTestUtils",
-            path: "AlfieKit/Common/TestUtils"
-        ),
-        .testTarget(
-            name: "CoreTests",
-            dependencies: [
-                "Core",
-                "Mocks",
-                "Common",
-                "CommonTestUtils",
-                .product(name: "BFFGraphMocks", package: "BFFGraphApi"),
-                .product(name: "Apollo", package: "apollo-ios"),
-            ],
-            path: "AlfieKit/Core/Tests/CoreTests"
-        ),
-        .testTarget(
-            name: "NavigationTests",
-            dependencies: [
-                "Navigation"
-            ],
-            path: "AlfieKit/Navigation/Tests/NavigationTests"
-        ),
-        .testTarget(
-            name: "StyleGuideTests",
-            dependencies: [
-                "StyleGuide",
-                "Common",
-                "Core"
-            ],
-            path: "AlfieKit/StyleGuide/Tests/StyleGuideTests"
-        )
     ]
+)
+
+// MARK: - Common -
+
+package.targets.append(
+    contentsOf:
+        [
+            .target(
+                name: "Common",
+                path: "AlfieKit/Common/Sources"
+            ),
+            .target(
+                name: "CommonTestUtils",
+                path: "AlfieKit/Common/TestUtils"
+            )
+        ]
+)
+
+package.products.append(
+    contentsOf:
+        [
+            .library(
+                name: "Common",
+                targets: ["Common"]
+            ),
+            .library(
+                name: "CommonTestUtils",
+                targets: ["CommonTestUtils"]
+            )
+        ]
+)
+
+// MARK: - Core -
+
+package.targets.append(
+    contentsOf:
+        [
+            .target(
+                name: "Core",
+                dependencies: [
+                    "Models",
+                    "Common",
+                    "BFFGraphApi",
+                    "EasyStash",
+                    .product(
+                        name: "FirebaseRemoteConfig",
+                        package: "firebase-ios-sdk"
+                    ),
+                    .product(
+                        name: "FirebaseCrashlytics",
+                        package: "firebase-ios-sdk"
+                    ),
+                    .product(
+                        name: "FirebaseAnalytics",
+                        package: "firebase-ios-sdk"
+                    ),
+                    .product(
+                        name: "NukeUI",
+                        package: "nuke"
+                    ),
+                    .product(
+                        name: "Apollo",
+                        package: "apollo-ios"
+                    ),
+                    .product(
+                        name: "BrazeKit",
+                        package: "braze-swift-sdk"
+                    ),
+                ],
+                path: "AlfieKit/Core/Sources"
+            ),
+            .testTarget(
+                name: "CoreTests",
+                dependencies: [
+                    "Core",
+                    "Mocks",
+                    "Common",
+                    "CommonTestUtils",
+                    .product(
+                        name: "BFFGraphMocks",
+                        package: "BFFGraphApi"
+                    ),
+                    .product(
+                        name: "Apollo",
+                        package: "apollo-ios"
+                    ),
+                ],
+                path: "AlfieKit/Core/Tests/CoreTests"
+            )
+        ]
+)
+
+package.products.append(
+    contentsOf:
+        [
+            .library(
+                name: "Core",
+                targets: ["Core"]
+            )
+        ]
+)
+
+// MARK: - Mocks -
+
+package.targets.append(
+    contentsOf:
+        [
+            .target(
+                name: "Mocks",
+                dependencies: [
+                    "Models"
+                ],
+                path: "AlfieKit/Mocks/Sources"
+            )
+        ]
+)
+
+package.products.append(
+    contentsOf:
+        [
+            .library(
+                name: "Mocks",
+                targets: ["Mocks"]
+            )
+        ]
+)
+
+// MARK: - Models -
+
+package.targets.append(
+    contentsOf:
+        [
+            .target(
+                name: "Models",
+                dependencies: [
+                    .product(
+                        name: "OrderedCollections",
+                        package: "swift-collections"
+                    ),
+                ],
+                path: "AlfieKit/Models/Sources",
+                resources: []
+            )
+        ]
+)
+
+package.products.append(
+    contentsOf:
+        [
+            .library(
+                name: "Models",
+                targets: ["Models"]
+            )
+        ]
+)
+
+// MARK: - Navigation -
+
+package.targets.append(
+    contentsOf:
+        [
+            .target(
+                name: "Navigation",
+                path: "AlfieKit/Navigation/Sources"
+            ),
+            .testTarget(
+                name: "NavigationTests",
+                dependencies: [
+                    "Navigation"
+                ],
+                path: "AlfieKit/Navigation/Tests/NavigationTests"
+            )
+        ]
+)
+
+package.products.append(
+    contentsOf:
+        [
+            .library(
+                name: "Navigation",
+                targets: ["Navigation"]
+            )
+        ]
+)
+
+// MARK: - StyleGuide -
+
+package.targets.append(
+    contentsOf:
+        [
+            .target(
+                name: "StyleGuide",
+                dependencies: [
+                    "Navigation",
+                    "Common",
+                    "Core",
+                    "Models"
+                ],
+                path: "AlfieKit/StyleGuide/Sources",
+                resources: [
+                    .copy("Theme/Typography/Resources/SF-Pro-Display-Medium.otf"),
+                    .copy("Theme/Components/Loader/spin.gif"),
+                    .process("Theme/Typography/Resources/Fonts.xcassets"),
+                    .process("Theme/Color/Colors.xcassets"),
+                    .process("Theme/Images/ThemedImages.xcassets"),
+                    .process("Theme/Toggle/ToggleColor.xcassets")
+                ]
+            ),
+            .testTarget(
+                name: "StyleGuideTests",
+                dependencies: [
+                    "StyleGuide",
+                    "Common",
+                    "Core"
+                ],
+                path: "AlfieKit/StyleGuide/Tests/StyleGuideTests"
+            )
+        ]
+)
+
+package.products.append(
+    contentsOf:
+        [
+            .library(
+                name: "StyleGuide",
+                targets: ["StyleGuide"]
+            )
+        ]
 )
