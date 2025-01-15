ef1e713d526d3bb1cb8585bdeafe2d5ac4a507b3
Daniel Primo <>
2025-01-15 11:55:31 +0000

    % code review fix

--- Alfie/AlfieKit/Package.swift
+++ Alfie/AlfieKit/Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -10,7 +9,6 @@ let package = Package(
         .macOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "BFFGraphApi",
             targets: ["BFFGraphApi"]
@@ -49,36 +47,14 @@ let package = Package(
         )
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(
-            url: "https://github.com/firebase/firebase-ios-sdk.git",
-            exact: "10.20.0"
-        ),
-        .package(
-            url: "https://github.com/kean/Nuke.git",
-            exact: "12.4.0"
-        ),
-        .package(
-            url: "https://github.com/apollographql/apollo-ios.git",
-            exact: "1.7.1"
-        ),
-        .package(
-            url: "https://github.com/onmyway133/EasyStash.git",
-            exact: "1.1.9"
-        ),
-        .package(
-            url: "https://github.com/braze-inc/braze-swift-sdk",
-            exact: "8.0.1"
-        ),
-        .package(
-            url: "https://github.com/apple/swift-collections",
-            from: "1.0.6"
-        )
+        .package(url: "https://github.com/apollographql/apollo-ios.git", exact: "1.7.1"),
+        .package(url: "https://github.com/braze-inc/braze-swift-sdk", exact: "8.0.1"),
+        .package(url: "https://github.com/onmyway133/EasyStash.git", exact: "1.1.9"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "10.20.0"),
+        .package(url: "https://github.com/kean/Nuke.git", exact: "12.4.0"),
+        .package(url: "https://github.com/apple/swift-collections", exact: "1.0.6")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        
         .target(
             name: "BFFGraphApi",
             dependencies: [
@@ -102,34 +78,16 @@ let package = Package(
         .target(
             name: "Core",
             dependencies: [
-                "Models",
-                "Common",
                 "BFFGraphApi",
+                "Common",
                 "EasyStash",
-                .product(
-                    name: "FirebaseRemoteConfig",
-                    package: "firebase-ios-sdk"
-                ),
-                .product(
-                    name: "FirebaseCrashlytics",
-                    package: "firebase-ios-sdk"
-                ),
-                .product(
-                    name: "FirebaseAnalytics",
-                    package: "firebase-ios-sdk"
-                ),
-                .product(
-                    name: "NukeUI",
-                    package: "nuke"
-                ),
-                .product(
-                    name: "Apollo",
-                    package: "apollo-ios"
-                ),
-                .product(
-                    name: "BrazeKit",
-                    package: "braze-swift-sdk"
-                ),
+                "Models",
+                .product(name: "Apollo", package: "apollo-ios"),
+                .product(name: "BrazeKit", package: "braze-swift-sdk"),
+                .product(name: "FirebaseRemoteConfig",package: "firebase-ios-sdk"),
+                .product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
+                .product(name: "NukeUI", package: "nuke")
             ]
         ),
         
@@ -143,10 +101,7 @@ let package = Package(
         .target(
             name: "Models",
             dependencies: [
-                .product(
-                    name: "OrderedCollections",
-                    package: "swift-collections"
-                ),
+                .product(name: "OrderedCollections", package: "swift-collections"),
             ]
         ),
         
@@ -157,16 +112,16 @@ let package = Package(
         .target(
             name: "StyleGuide",
             dependencies: [
-                "Navigation",
                 "Common",
                 "Core",
-                "Models"
+                "Models",
+                "Navigation"
             ],
             resources: [
                 .copy("Theme/Typography/Resources/SF-Pro-Display-Medium.otf"),
                 .copy("Theme/Components/Loader/spin.gif"),
-                .process("Theme/Typography/Resources/Fonts.xcassets"),
                 .process("Theme/Color/Colors.xcassets"),
+                .process("Theme/Typography/Resources/Fonts.xcassets"),
                 .process("Theme/Images/ThemedImages.xcassets"),
                 .process("Theme/Toggle/ToggleColor.xcassets")
             ]
@@ -179,15 +134,12 @@ let package = Package(
         .testTarget(
             name: "CoreTests",
             dependencies: [
+                "BFFGraphMocks",
+                "Common",
                 "Core",
                 "Mocks",
-                "Common",
                 "TestUtils",
-                "BFFGraphMocks",
-                .product(
-                    name: "Apollo",
-                    package: "apollo-ios"
-                ),
+                .product(name: "Apollo", package: "apollo-ios"),
             ]
         ),
         .testTarget(
@@ -199,9 +151,9 @@ let package = Package(
         .testTarget(
             name: "StyleGuideTests",
             dependencies: [
-                "StyleGuide",
                 "Common",
-                "Core"
+                "Core",
+                "StyleGuide"
             ]
         )
     ]
