f8da98227481ed91db4aab3d8bb770da7cc20af3
João Pinho <joao.pinho@mindera.com>
2025-03-18 11:39:57 +0000

    ALFMOB-161: Incorrect Wishlist Header when opened from bottom bar
    The **Wishlist toolbar** was being defined in `WishlistView`, causing it to always use the same header. regardless of how the view was opened.
    
    **How it was fixed:**
    - Moved toolbar definition to `ViewFactory`, ensuring the correct header is applied based on whether the view is opened from the **bottom bar** or via a **push event**.

--- Alfie/Alfie/Navigation/ViewFactory.swift
+++ Alfie/Alfie/Navigation/ViewFactory.swift
@@ -48,6 +48,7 @@ final class ViewFactory: ViewFactoryProtocol {
 
             case .wishlist:
                 wishlistView
+                    .withToolbar(for: .tab(.wishlist))
 
             case .bag:
                 BagView(
@@ -127,6 +128,7 @@ final class ViewFactory: ViewFactoryProtocol {
 
         case .wishlist:
             wishlistView
+                .withToolbar(for: .wishlist)
 
         case .productDetails(let type):
             switch type {
